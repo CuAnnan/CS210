6d6a1bc5f0af8431b75db2c0a06159002a9bb846
Wing <wing@busschots.ie>
2024-11-02 17:38:42 +0000

    Properly commented Linked List.

---
+++ src/LinkedList/Driver.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        for(int i = 0; i < 20; i++)
+        {
+            l.add(i);
+        }
+        try
+        {
+            LinkedList<Integer> l4 = l.slice(0);
+            l.debug();
+            l4.debug();
+            l4.set(6, 78);
+            l.debug();
+            l4.debug();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace(System.out);
+        }
+    }
+
+}
