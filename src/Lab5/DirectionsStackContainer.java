a48d1b19fc15508cdf843cfb6997c0d30d3a32a6
wing <wing@busschots.ie>
2024-11-05 10:16:07 +0000

    Cleanup

--- src/Lab5/DirectionsStackContainer.java
+++ src/Lab5/DirectionsStackContainer.java
@@ -84,7 +84,12 @@ public class DirectionsStackContainer
 	
 	public static void main(String args[])
 	{
-		
+		DirectionsStackContainer dc = new DirectionsStackContainer();
+		String[] directionsSet = "Go North;Go North;Go West;Go Back;Go East;Go South;Go North;Go Back;Arrived".split(";");
+		for(String direction: directionsSet)
+		{
+			dc.addInstruction(direction);
+		}
 	}
 	
 	/**
