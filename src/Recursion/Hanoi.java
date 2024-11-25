9b163688623a91104711c12bf1ede1ef6efa3f3c
Wing <wing@busschots.ie>
2024-11-25 22:14:08 +0000

    Hanoi done

--- src/Recursion/Hanoi.java
+++ src/Recursion/Hanoi.java
@@ -1,63 +1,80 @@
 package Recursion;
 
-class HanoiDisc
-{
-    char label;
-    
-    public char getLabel()
-    {
-        return label;
-    }
 
-    public void setLabel(char label)
-    {
-        this.label = label;
-    }
-
-    public HanoiDisc(char label)
-    {
-        this.label = label;
-    }
-}
-
+/**
+ * Very primitive stack class to handle the towers themselves. This *could* have been done with just arrays but this provided some nice debugging potential.
+ * @author Éamonn "Wing" Kearns 60460770
+ */
 class Tower
 {
-	HanoiDisc[] discs;
+    /**
+     * The chars representing the named tiles being stored
+     */
+	char[] discs;
+	/**
+	 * The number of tiles on this stack, used for rendering
+	 */
 	int length;
+	/**
+	 * The name of the tower (used for debugging)
+	 */
 	String name;
 	
+	/**
+	 * Constructor
+	 * @param name The name of the tower
+	 * @param size The maximum size of the tower. Used to instantiate the array.
+	 */
 	public Tower(String name, int size)
 	{
-	    this.discs = new HanoiDisc[size];
+	    this.discs = new char[size];
 	    this.name = name;
 		this.length = 0;
 	}
 	
-	public void addDisc(HanoiDisc disc)
+	/**
+	 * Adder. Adds to the last element of the array.
+	 * @param disc
+	 */
+	public void addDisc(char disc)
 	{
 		this.discs[this.length++] = disc;
 	}
 	
-	public HanoiDisc getDisc(int i)
+	/**
+	 * Getter for an individual index
+	 * @param i
+	 * @return
+	 */
+	public char getDisc(int i)
 	{
 		return this.discs[i];
 	}
 	
-	public HanoiDisc removeTopDisc()
+	/**
+	 * Pop a disc off the last space
+	 * @return
+	 */
+	public char removeTopDisc()
 	{
 		return this.discs[--this.length];
 	}
 
+	/**
+	 * Get the name
+	 * @return
+	 */
     public String getName()
     {
         return name;
     }
 
-    public void setName(String name)
-    {
-        this.name = name;
-    }
 }
+
+/**
+ * Towers of Hanoi simulator. Prints out each stage. Done as part of the Recursion part of the lectures.
+ * @author Éamonn "Wing" Kearns 60460770
+ */
 
 public class Hanoi
 {
@@ -68,24 +85,41 @@ public class Hanoi
     int size;
     int lastPrintLength;
     
+    /**
+     * This throws an exception if you run out of characters from the alphabet, putting a hard cap of 26 tiles on the tower.
+     * But given that would be O(2^26), I'm okay with that. 
+     * @param size
+     */
     public Hanoi(int size)
     {
+        /*
+         * Instantiate out towers
+         */
         towers[0] = new Tower("Tower 1", size);
         towers[1] = new Tower("Tower 2", size);
         towers[2] = new Tower("Tower 3", size);
         
+        /*
+         * Store the size as a property, we use this in the print method
+         */
         this.size = size;
         
-        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        
+        /*
+         * Name the towers alphabetically
+         */
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        /**
+         * populate the first tower
+         */
         for(int i = 0; i < size; i ++)
         {
-            towers[0].addDisc(new HanoiDisc(alphabet.charAt(size - i - 1)));
+            towers[0].addDisc(alphabet.charAt(size - i - 1));
         }
-        
-        
     }
     
+    /**
+     * Solve kickstarter. Calls swap with the original tower and full size
+     */
     public void solve()
     {
         System.out.println("Starting state:");
@@ -95,36 +129,46 @@ public class Hanoi
     	this.print();
     }
     
+    /**
+     * @param n             This is used as the simplest possible anchor case. It also helps with output
+     * @param src           The tower to move the tile from
+     * @param intermediate  The tower to use as a swap space
+     * @param destination   The tower to end the substack on
+     */
     public void swap(int n, Tower src, Tower intermediate, Tower destination)
     {
+        // base case
         if(n == 0)
     	{
             return;
     	}
+        // swap a substack to the intermediate tower
         swap(n - 1, src, destination, intermediate);
+        // Do the swap of the single tile
         destination.addDisc(src.removeTopDisc());
+        // print out the state
         this.print();
+        // swap it to its destination
         swap(n - 1, intermediate, src, destination);
-    	
     }
     
     
     
     public void print()
     {
-        
-        String state = "    *         *         *\n";
+        String state="    *         *         *\n";
         
         for(int i = this.size - 1; i >=0; i --)
         {
             for(int j = 0; j < 3; j++)
             {
-                state +=  "   "+(towers[j].length <= i ? " * " : "["+towers[j].getDisc(i).label+"]")+"    ";
+                state +=  "   "+(towers[j].length <= i ? " * " : "["+towers[j].getDisc(i)+"]")+"    ";
             }
             state += "\n";
             
         }
         state += " *******   *******   *******\n";
+        
         System.out.println(state);
         this.lastPrintLength = state.length();
     }
